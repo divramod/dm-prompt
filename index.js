bb1b5bfc89723ba75f320e22b5f0bea67b68a98f
divramod <divramod@gmail.com>
2015-07-28 05:34:55 +0200

    added yielded inquirer prompt

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-var colors = require("colors");
+var inquirer = require("inquirer");
+var Promise = require('bluebird');
+var _ = require('lodash');
 var co = require("co");
-require("shelljs/global");
-var jobs = {};
-var result = {};
-var module_path = __dirname;
 
-// =========== [ job.index() ] ===========
-jobs.index = co.wrap(function*() {
+var job = {};
 
-    // =========== [ get params from user input ] ===========
-    result.job = process.env.dmnJob || process.argv[2] || "help";
 
-    // =========== [ help ] ===========
-    if (result.job === "help" || result.job === "-h" || result.job === "-help") {
-        var task = require("./tasks/help/index.js");
-        yield task.start(module_path);
-    }
-    // =========== [ test ] ===========
-    else if (["test", "t", "-t"].indexOf(result.job) > -1) {
-        var task = require("./tasks/test/index.js");
-        yield task.start();
-    }
-    // =========== [ help ] ===========
-    else {
-        var task = require("./tasks/help/index.js");
-        yield task.start(module_path);
-    }
+// =========== [ inquirer promising ] ===========
+// http://blog.paperelectron.com/promise-based-inquirer-workflow/
+job.start = function Prompt(prompt) {
+    return new Promise(function(resolve, reject) {
+        inquirer.prompt(prompt, function(answer) {
+            resolve(answer);
+        });
+    });
+}
 
-    return Promise.resolve(result);
-}); // job.index()
-
-// =========== [ MODULE EXPORT ] ===========
-module.exports = jobs;
+// =========== [ job.run ] ===========
+module.exports = co.wrap(function*(questions) {
+    return yield job.start(questions)
+        .bind({}) // A blank object for 'this'
+        .then(function(choices) {
+            return choices;
+        });
+}); // job.run
